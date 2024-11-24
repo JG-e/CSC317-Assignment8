ae7c9dba2b263df0882382667f61efbcbaa52329
JG-e <zezhengh@163.com>
2024-11-23 21:25:46 -0500

    Implement signed incidence matrix calculation in dense format

--- src/signed_incidence_matrix_dense.cpp
+++ src/signed_incidence_matrix_dense.cpp
@@ -1,12 +1,18 @@
 #include "signed_incidence_matrix_dense.h"
-
+using namespace std;
+using namespace Eigen;
 void signed_incidence_matrix_dense(
   const int n,
   const Eigen::MatrixXi & E,
   Eigen::MatrixXd & A)
 {
   //////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
   A = Eigen::MatrixXd::Zero(E.rows(),n);
+  for(int e = 0; e<E.rows(); e++)
+  {
+    A(e, E(e,0)) += 1;
+    A(e, E(e,1)) += -1;
+  }
+  
   //////////////////////////////////////////////////////////////////////////////
 }
