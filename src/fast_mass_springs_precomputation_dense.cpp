d214c5d42b6d525fe532283f38b3fcfe2caab0f4
JG-e <zezhengh@163.com>
2024-11-24 20:56:35 -0500

    Enhance fast mass springs precomputation with signed incidence matrix and stiffness matrix adjustments

--- src/fast_mass_springs_precomputation_dense.cpp
+++ src/fast_mass_springs_precomputation_dense.cpp
@@ -1,6 +1,8 @@
 #include "fast_mass_springs_precomputation_dense.h"
 #include "signed_incidence_matrix_dense.h"
 #include <Eigen/Dense>
+#define w 1e10
+using namespace Eigen;
 
 bool fast_mass_springs_precomputation_dense(
   const Eigen::MatrixXd & V,
@@ -16,8 +18,30 @@ bool fast_mass_springs_precomputation_dense(
   Eigen::LLT<Eigen::MatrixXd> & prefactorization)
 {
   /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
+
   Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(V.rows(),V.rows());
+
+  signed_incidence_matrix_dense(V.rows(), E, A);
+
+  M = Eigen::MatrixXd::Zero(V.rows(),V.rows());
+
+  C = Eigen::MatrixXd::Zero(b.rows(), V.rows());
+  for (int i = 0; i < b.rows(); i++) {
+    C(i, b(i)) = 1;
+  }
+
+  r.resize(E.rows());
+  for (int i = 0; i < E.rows(); i++) {
+    auto V0 = V.row( E(i, 0) );
+    auto V1 = V.row( E(i, 1) );
+    r(i) = (V0 - V1).norm();
+  }
+
+  M.diagonal() += m;
+
+  Q = k * A.transpose() * A + M / pow(delta_t,2);
+  MatrixXd Qc = w * C.transpose() * C; // Qc = wCᵀC
+  Q += Qc;
   /////////////////////////////////////////////////////////////////////////////
   prefactorization.compute(Q);
   return prefactorization.info() != Eigen::NumericalIssue;
