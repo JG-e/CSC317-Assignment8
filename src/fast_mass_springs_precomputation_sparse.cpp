d01afaa30c1adae9e654b04b6af2064380518834
JG-e <zezhengh@163.com>
2024-11-25 22:19:31 -0500

    Integrate signed incidence matrix calculation into sparse precomputation and optimize matrix assembly

--- src/fast_mass_springs_precomputation_sparse.cpp
+++ src/fast_mass_springs_precomputation_sparse.cpp
@@ -1,6 +1,7 @@
 #include "fast_mass_springs_precomputation_sparse.h"
 #include "signed_incidence_matrix_sparse.h"
 #include <vector>
+#define w 1e10
 
 bool fast_mass_springs_precomputation_sparse(
   const Eigen::MatrixXd & V,
@@ -16,12 +17,31 @@ bool fast_mass_springs_precomputation_sparse(
   Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > & prefactorization)
 {
   /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
+  signed_incidence_matrix_sparse(V.rows(), E, A);
   std::vector<Eigen::Triplet<double> > ijv;
+
   const int n = V.rows();
-  for(int i = 0;i<n;i++) ijv.emplace_back(i,i,1);
-  Eigen::SparseMatrix<double> Q(n,n);
-  Q.setFromTriplets(ijv.begin(),ijv.end());
+
+  for(int i = 0;i<b.rows();i++) ijv.emplace_back(i,b(i),1);
+  C.setFromTriplets(ijv.begin(),ijv.end());
+  ijv.clear();
+
+  r.resize(E.rows());
+  for (int i = 0; i < E.rows(); i++) {
+    auto V0 = V.row( E(i, 0) );
+    auto V1 = V.row( E(i, 1) );
+    r(i) = (V0 - V1).norm();
+  }
+
+  M.resize(n,n);
+
+  for(int i = 0;i<n;i++) ijv.emplace_back(i,i,m(i));
+  M.setFromTriplets(ijv.begin(),ijv.end());
+  ijv.clear();
+
+  Eigen::SparseMatrix<double> Q = k * A.transpose() * A + M / pow(delta_t,2);
+  Eigen::SparseMatrix<double> Qc = w * C.transpose() * C; // Qc = wCᵀC
+  Q += Qc;
   /////////////////////////////////////////////////////////////////////////////
   prefactorization.compute(Q);
   return prefactorization.info() != Eigen::NumericalIssue;
